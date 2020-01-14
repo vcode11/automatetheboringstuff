d36b04873bea30e142d2f33640d0c29074c89cf4
Vishal Mishra <vishalm434@gmail.com>
2020-01-15 01:13:11 +0530

    Add docker set up foor proxies

--- set_proxy.py
+++ set_proxy.py
@@ -1,9 +1,5 @@
-''' Filename: set_proxy.py 
- Author: Vishal Mishra
- Purpose: Sets a proxy in Ubuntu
- Usage: sudo python3 set_proxy.py
- '''
 import os
+import sys
 from getpass import getpass
 
 address = input('Enter the proxy address:')
@@ -13,13 +9,35 @@ port = int(input('Enter port:'))
 username = input('Enter Username:')
 password = getpass(prompt='Password:')
 
-os.chdir('/etc/apt/')
-with open('apt.conf','w') as f:
+#For apt software centre
+with open('/etc/apt/apt.conf','w') as f:
     verbs = ['http','https','ftp']
     for verb in verbs:
         f.write(f'Acquire::{verb}::proxy "{verb}://{username}:{password}@{address}:{port}/";\n')
-os.chdir('/etc')
-with open('etc/environment','a') as f:
-    verbs = ['http','https','ftp']
-    for verb in verbs:
-        f.write(f'{verb}_proxy={verb}://{username}:{password}@{address}:{port}/\n')
+#For docker
+os.makedirs('/etc/systemd/system/docker.service.d',exist_ok=True)
+os.makedirs('/etc/default/',exist_ok=True)
+with open('/etc/systemd/system/docker.service.d/http-proxy.conf','w') as f:
+    f.write(f'''[Service]
+Environment="HTTP_PROXY=http://{address}:{port}"
+Environment="HTTPS_PROXY=http://{address}:{port}"
+Environment="NO_PROXY=localhost,127.0.0.1"''')
+
+
+with open('/etc/default/docker','w') as f:
+    f.write(f'''[Service]
+Environment="HTTP_PROXY=http://{address}:{port}"
+Environment="HTTPS_PROXY=http://{address}:{port}"
+Environment="NO_PROXY=localhost,127.0.0.1"''')
+
+#For git etc...
+content = []
+with open('/etc/environment') as f:
+    content = f.readlines()
+    content = list(filter(lambda s: 'proxy' not  in s, content))
+    with open('/etc/environment','w') as f:
+        for line in content:
+            f.write(line)
+        verbs = ['http','https','ftp']
+        for verb in verbs:
+            f.write(f'{verb}_proxy={verb}://{username}:{password}@{address}:{port}/\n')
